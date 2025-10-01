822c36e0c900465cacbc4cc02f8c7fa4318ea2b0
isibea <is.bascunan@duocuc.cl>
2025-10-01 17:49:19 -0300

    Migración a Empty Compose Activity avances hasta la guia 9

---
+++ app/src/main/java/com/evaluacion/a0waste_G5_final/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.evaluacion.a0waste_G5_final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.evaluacion.a0waste_G5_final.ui.theme._0waste_G5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _0waste_G5Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _0waste_G5Theme {
+        Greeting("Android")
+    }
+}
