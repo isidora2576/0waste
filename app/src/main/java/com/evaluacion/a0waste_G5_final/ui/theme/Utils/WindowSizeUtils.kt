76fc61d67694cffc7b609de4a7a9c7498db41fb2
isibea <is.bascunan@duocuc.cl>
2025-10-03 00:04:22 -0300

    se avanzo en la guia 9 aun falta por completarlo y adaptarlo a nuestra app

---
+++ app/src/main/java/com/evaluacion/a0waste_G5_final/ui/theme/Utils/WindowSizeUtils.kt
@@ -0,0 +1,6 @@
+package com.evaluacion.a0waste_G5_final.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
