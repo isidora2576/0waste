109331342f94c6dc5f399f04d45f2714280388c7
Jesus <jes.moran@gmail.com>
2025-10-27 03:27:35 -0300

    WasteView actualizado para persistencia de datos

--- app/src/main/java/com/evaluacion/a0waste_G5_final/Viewmodel/WasteViewModel.kt
+++ app/src/main/java/com/evaluacion/a0waste_G5_final/Viewmodel/WasteViewModel.kt
@@ -1,28 +1,65 @@
 package com.evaluacion.a0waste_G5_final.Viewmodel
 
-
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.test.core.app.ApplicationProvider
+import com.evaluacion.a0waste_G5_final.Data.AppDataStore
+import com.evaluacion.a0waste_G5_final.Navigation.NavigationEvent
 import com.evaluacion.a0waste_G5_final.Navigation.Screen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import com.evaluacion.a0waste_G5_final.Navigation.NavigationEvent
 
-open class WasteViewModel() : ViewModel() {
+open class WasteViewModel(application: Application) : AndroidViewModel(application) {
 
-    private var userPoints = 0
-    open fun addPoints(points: Int) {
-        userPoints += points
+    private val appDataStore = AppDataStore(application.applicationContext)
+
+    val _puntosUsuario = MutableStateFlow<Int?>(null)
+    val puntosUsuario: StateFlow<Int?> = _puntosUsuario
+
+    private val _mostrarMensajePuntos = MutableStateFlow(false)
+    val mostrarMensajePuntos: StateFlow<Boolean> = _mostrarMensajePuntos
+
+    init {
+        cargarPuntos()
     }
-    open fun getPoints(): Int = userPoints
+
+    private fun cargarPuntos() {
+        viewModelScope.launch {
+            delay(1500)
+            _puntosUsuario.value = appDataStore.getUserPoints().first()
+        }
+    }
+
+
+    fun agregarPuntos(puntos: Int) {
+        viewModelScope.launch {
+            appDataStore.addUserPoints(puntos)
+
+            _puntosUsuario.value = appDataStore.getUserPoints().first()
+
+            _mostrarMensajePuntos.value = true
+
+            delay(2000)
+
+            _mostrarMensajePuntos.value = false
+        }
+    }
+
+    fun getPoints(): Int = _puntosUsuario.value ?: 0
 
     private val _navigationEvents = MutableSharedFlow<NavigationEvent>()
     val navigationEvents = _navigationEvents.asSharedFlow()
 
     fun navigateTo(screen: Screen) {
-        CoroutineScope(context = Dispatchers.Main).launch  {
+        CoroutineScope(context = Dispatchers.Main).launch {
             _navigationEvents.emit(NavigationEvent.NavigateTo(route = screen))
         }
     }
@@ -36,6 +73,18 @@ open class WasteViewModel() : ViewModel() {
     fun navigateUp() {
         CoroutineScope(context = Dispatchers.Main).launch {
             _navigationEvents.emit(value = NavigationEvent.NavigateUp)
+        }
+    }
+}
+
+
+class PreviewWasteViewModel : WasteViewModel(
+    application = ApplicationProvider.getApplicationContext() as Application
+) {
+    init {
+
+        viewModelScope.launch {
+            _puntosUsuario.value = 0
         }
     }
 }
