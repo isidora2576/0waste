560a7891e2db9af013ae7a3265617940b1de97e7
Jesus <jes.moran@gmail.com>
2025-10-27 03:13:47 -0300

    Persistencia de datos local para funciones, acumulación de puntos en usuario demo y modo eco

--- app/src/main/java/com/evaluacion/a0waste_G5_final/Data/EstadoDataStore.kt
+++ app/src/main/java/com/evaluacion/a0waste_G5_final/Data/EstadoDataStore.kt
@@ -1,2 +1,17 @@
 package com.evaluacion.a0waste_G5_final.Data
 
+import android.content.Context
+import kotlinx.coroutines.flow.Flow
+
+class EstadoDataStore(private val context: Context) {
+
+    private val appDataStore = AppDataStore(context)
+
+    suspend fun guardarEstado(valor: Boolean) {
+        appDataStore.setEcoMode(valor)
+    }
+
+    fun obtenerEstado(): Flow<Boolean> {
+        return appDataStore.getEcoMode()
+    }
+}
